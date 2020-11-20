007b27ad442c87c2890f1d9624aec3ae26ac1913
hau1112 <trandinhhauac@gmail.com>
2020-11-20 10:51:57 +0700

    làm layout + Intend qua đăng ký :))

--- app/src/main/java/com/example/datlichkham/DangNhapActivity.java
+++ app/src/main/java/com/example/datlichkham/DangNhapActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DangNhapActivity extends AppCompatActivity {
-
+    Button btnDangky;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
-
+        btnDangky = findViewById(R.id.dangNhap_btnDangky);
+        btnDangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DangNhapActivity.this, DangKiActivity.class);
+                startActivity(intent);
+            }
+        });
         Boolean isFirstLogin = true;
         if(isFirstLogin)
         {
