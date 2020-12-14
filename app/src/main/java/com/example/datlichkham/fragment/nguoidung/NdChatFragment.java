47f5de4f347183644b3a852a0529f3a15a8f65c4
datbong <nongtrainuoikhikute@gmail.com>
2020-12-14 23:17:19 +0700

    cập nhật chức năng tìm kiếm người dùng theo Id người dùng trong chat

--- app/src/main/java/com/example/datlichkham/fragment/nguoidung/NdChatFragment.java
+++ app/src/main/java/com/example/datlichkham/fragment/nguoidung/NdChatFragment.java
@@ -8,9 +8,12 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.datlichkham.R;
 import com.example.datlichkham.adapter.UserChatAdapter;
@@ -19,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -35,6 +39,8 @@ public class NdChatFragment extends Fragment {
     private List<Users> usersList;
 
     private String myId;
+
+    private EditText searchBar;
 
     public NdChatFragment() {
         // Required empty public constructor
@@ -59,7 +65,51 @@ public class NdChatFragment extends Fragment {
         usersList = new ArrayList<>();
         myId = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE).getString("USERNAME", "");
         getUserFromDb();
+
+        searchBar = view.findViewById(R.id.searchBar);
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUser(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         return view;
+    }
+
+    private void searchUser(String s) {
+        Query query = FirebaseDatabase.getInstance().getReference("users").orderByChild("userName")
+                .startAt(s)
+                .endAt(s + "\uf8ff");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usersList.clear();
+                Users users = new Users();
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    users = ds.getValue(Users.class);
+                    if(!users.getUserName().equalsIgnoreCase(myId)){
+                        usersList.add(users);
+                    }
+                }
+                UserChatAdapter userChatAdapter = new UserChatAdapter(getContext(), usersList);
+                rcUser.setAdapter(userChatAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void getUserFromDb() {
@@ -67,6 +117,7 @@ public class NdChatFragment extends Fragment {
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usersList.clear();
                 Users users = new Users();
                 for(DataSnapshot ds: dataSnapshot.getChildren()){
                     users = ds.getValue(Users.class);
